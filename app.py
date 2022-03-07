32039de71f7eeee21bb97053a5c507b887d59506
thysk <timx2tt@hotmail.co.uk>
2022-03-07 15:44:33 +0000

    Or replace

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ def create_primes_table():
     with DatabaseConnection('primes.db') as connection:
         cursor = connection.cursor()
 
-        cursor.execute('CREATE TABLE IF NOT EXISTS primes (prime_number INTEGER PRIMARY KEY)')
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS primes (prime_number INTEGER PRIMARY KEY)')
         val = 2
-        cursor.execute(f"INSERT INTO primes (prime_number) VALUES ({val})")
+        cursor.execute(
+            f"INSERT OR REPLACE INTO primes (prime_number) VALUES ({val})")
 
 
 def prime_generator(upper_bound, my_highest_prime):
@@ -59,7 +61,8 @@ def find_primes():
     with DatabaseConnection('primes.db') as connection:
         cursor = connection.cursor()
 
-        cursor.execute('SELECT prime_number FROM primes ORDER BY prime_number DESC')
+        cursor.execute(
+            'SELECT prime_number FROM primes ORDER BY prime_number DESC')
         my_highest_prime = cursor.fetchone() or 2
         if my_highest_prime != 2:
             my_highest_prime = (my_highest_prime[0]) + 1
@@ -69,7 +72,6 @@ def find_primes():
     write = prime_generator(upper_bound, my_highest_prime)
     for n in write:
         write_primes_to_db(n)
-
 
 
 user_options = {
