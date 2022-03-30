58f3625c9d76141e12bd1de42aef5d651766776b
Thysk <timx2tt@hotmail.co.uk>
2022-03-30 13:04:24 +0100

    Dev (#24)
    * reworked flow to prevent need for exeption
    
    * Added simple timer to help find optimal searching
    
    * implemented sieve of eratosthenes
    
    * Included the low for continued searching later
    
    * changed DB fix to find or reaplace
    
    * Timer re applied
    
    * Moved end to before return statement
    
    * Or replace
    
    * Re added imports
    
    * 1
    
    * fix a bug with the DB that caused crashes each run
    
    * removed dict when reading  DB with redundant label
    
    * Moved for loop to not create connection each time
    
    * include a loop to prevent invalid user input
    
    * Comment and format code (#17)
    
    * commented code, and moved some variables into func
    
    * Commented code and formatting lines
    
    * Initial unit tests (#19)
    
    * Dev (#14)
    
    * reworked flow to prevent need for exeption
    
    * Added simple timer to help find optimal searching
    
    * implemented sieve of eratosthenes
    
    * Included the low for continued searching later
    
    * changed DB fix to find or reaplace
    
    * Timer re applied
    
    * Moved end to before return statement
    
    * Or replace
    
    * Re added imports
    
    * fix a bug with the DB that caused crashes each run
    
    * removed dict when reading  DB with redundant label
    
    * Moved for loop to not create connection each time
    
    * add initial unit tests
    
    * removed a db connection that was not needed
    
    * Added tests for DB reading + writing
    
    * adds app tests and adaprts app for calling from another module

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import prime_find
 
 
 # Main loop for program, generates the CLI menu to interact with code.
-def menu():
+def app_menu(user_input: str = None, upper_bound: int = None):
     user_options = {
         'r': prime_find.return_primes,
         'f': prime_find.find_primes_workflow
@@ -11,20 +11,32 @@ def menu():
 'r' : Returns all primes found
 or
 'f' : Find more primes: """
-    user_input = input(USER_CHOICE)
 
-    while True:
-        if user_input in user_options:
-            if user_input == 'r':
-                # Print the returned primes, can be changed later to something else
-                selected_function = user_options[user_input]
-                print(selected_function())
-            else:
-                selected_function = user_options[user_input]
-                selected_function()
-        else:
-            print("Input not recognised, please try again.")
+# menu work around for automation
+    if user_input:
+        if user_input == 'r':
+            # Print the returned primes, can be changed later
+            selected_function = user_options[user_input]
+            return(selected_function())
+        elif user_input == 'f':
+            selected_function = user_options[user_input]
+            selected_function(upper_bound)
+    else:
         user_input = input(USER_CHOICE)
+        # Human menu in a loop
+        while True:
+            if user_input in user_options:
+                if user_input == 'r':
+                    # Print the returned primes, can be changed later
+                    selected_function = user_options[user_input]
+                    print(selected_function())
+                elif user_input == 'f':
+                    selected_function = user_options[user_input]
+                    selected_function()
+            else:
+                print("Input not recognised, please try again.")
+            user_input = input(USER_CHOICE)
 
 
-menu()
+if __name__ == "__main__":
+    app_menu()
