bb73d910b334a474a330557ef165bf9a205dbfea
thysk <timx2tt@hotmail.co.uk>
2022-11-22 16:14:33 +0000

    Commented on test for TestMenu

--- app_test.py
+++ app_test.py
@@ -4,14 +4,20 @@ from app import app_menu
 
 
 class TestMenu:
+    """ Tests for the main App CLI menu
+    """
 
     def test_read_db_using_menu(self):
+        """ Uses the CLI to read primes from new db.
+        """
         if os.path.isfile('primes.db'):
             os.remove('primes.db')
         read = app_menu('r')
         assert read == [2]
 
     def test_use_menu_to_find_primes_up_to_10(self):
+        """ Uses the CLI to find primes up to 10.
+        """
         if os.path.isfile('primes.db'):
             os.remove('primes.db')
         app_menu('f', 10)
